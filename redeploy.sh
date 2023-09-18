4600d91938310ff95490a31fd2ad466f1131f890
DevMiner <devminer@devminer.xyz>
2023-09-18 19:40:13 +0200

    chore: run container detached

--- redeploy.sh
+++ redeploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+CONTAINER_NAME="battlebit-telemetry"
+
 git pull
 docker build -t bbtel .
 
-docker rm -f battlebit-telemetry
-docker run --name battlebit-telemetry -p 65500:65500 -p 65501:65501 bbtel -verbose
+docker rm -f "$CONTAINER_NAME"
+docker run -d --name "$CONTAINER_NAME" -p 65500:65500 -p 65501:65501 bbtel -verbose
+docker logs -f "$CONTAINER_NAME"
